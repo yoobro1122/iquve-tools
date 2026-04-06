e6a9ffe052f05c99a536d733b1c157e23cea59f3
yoobro1122 <129762228+yoobro1122@users.noreply.github.com>
2026-04-06 19:36:34 +0900

    Update next.config.js

--- iquve-mailer/next.config.js
+++ iquve-mailer/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { serverActions: { bodySizeLimit: '10mb' } },
+  api: { bodyParser: { sizeLimit: '10mb' } },
 }
 module.exports = nextConfig
